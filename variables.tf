60a5386afd7f4293c6cdcdf4bfb9713e3e99ad06
duda30 <dudalombardi30@gmail.com>
2022-10-11 23:00:25 -0300

    criação das variáveis da infraestrutura

--- variables.tf
+++ variables.tf
@@ -6,8 +6,8 @@ variable "bloco" {
   type = list(string)
   default = [
     "192.168.0.0/24",
-    "192.168.1.0/24",
-    "192.168.2.0/24"
+    "192.168.5.0/24",
+    "192.168.6.0/24"
   ]
 }
 
@@ -21,6 +21,7 @@ variable "resource-tags" {
 }
 
 variable "vpc_id" {
+  type    = string
   default = "vpc-0b769f0fdcf9ef629"
 }
 
