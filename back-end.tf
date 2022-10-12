115432a0b238f7ab9fe2f02291c110f36fef12b0
duda30 <dudalombardi30@gmail.com>
2022-10-11 22:56:45 -0300

    configuraçao do arq. backend

--- back-end.tf
+++ back-end.tf
@@ -1,8 +1,8 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "my-bucket-mary-maria12312"
-#     key    = "desafio-jenkins/primeira-tentativa/terraform.tfstate"
-#     region = "us-east-2"
-    
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "my-bucket-mary-maria12312"
+    key    = "desafio-jenkins/primeira-tentativa/terraform.tfstate"
+    region = "us-east-2"
+
+  }
+}
