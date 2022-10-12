1678dffa4c935f0b0513914c696e8f14e6dbe607
duda30 <dudalombardi30@gmail.com>
2022-10-11 22:57:21 -0300

    configuraçao do arq. data

--- data.tf
+++ data.tf
@@ -4,18 +4,11 @@ data "aws_vpc" "id-vpc-data" {
 
 data "aws_ami" "ubuntu" {
   most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
+  
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-
-  owners = ["self"]
 }
 
 data "aws_eip" "id-eip" {
