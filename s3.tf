c558fcd96dd09dcf1b96f0643873e118b3cf70dc
duda30 <dudalombardi30@gmail.com>
2022-10-11 22:59:44 -0300

    criação de um bucket s3

--- s3.tf
+++ s3.tf
@@ -1,18 +1,19 @@
 resource "aws_s3_bucket" "bucket-backend" {
   bucket = "my-bucket-mary-maria12312"
-
-  versioning {
-    enabled = true
-  }
-
   tags = {
     for key, value in var.resource-tags :
     key => lower(value)
   }
-  
 }
 
 resource "aws_s3_bucket_acl" "acl-to-my-bucket" {
   bucket = aws_s3_bucket.bucket-backend.id
   acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "versioning-my-bucket" {
+  bucket = aws_s3_bucket.bucket-backend.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
