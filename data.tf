9db73e3e593be1c76e67bc669b73c23f1c95e6fe
duda30 <dudalombardi30@gmail.com>
2022-10-13 17:21:29 -0300

    news data sources adc

--- data.tf
+++ data.tf
@@ -4,7 +4,12 @@ data "aws_vpc" "id-vpc-data" {
 
 data "aws_ami" "ubuntu" {
   most_recent = true
-  
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
@@ -13,4 +18,9 @@ data "aws_ami" "ubuntu" {
 
 data "aws_eip" "id-eip" {
   id = var.eip_id
+}
+
+data "aws_route53_zone" "zona-pavan" {
+  name = "juliocesarpavan.com.br"
+  private_zone = false
 }
